fce8272a2f724468ee26cf469f07908a81e9db4e
YeonginKim <fhtmah12@gmail.com>
2024-10-04 09:54:46 +0900

    library: Validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,13 @@ plugins {
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("plugin.jpa") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.22"
+
 }
 
+allOpen {
+    annotation("jakarta.persistence.Entity")
+}
 group = "com.lecture.special"
 version = "0.0.1-SNAPSHOT"
 
@@ -23,9 +28,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
