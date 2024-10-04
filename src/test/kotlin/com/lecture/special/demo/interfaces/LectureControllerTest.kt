3b95b17bbf58328d31557d80b9c22318fc70519e
YeonginKim <fhtmah12@gmail.com>
2024-10-04 09:59:00 +0900

    test: 동일 유저 수강 신청 케이스

---
+++ src/test/kotlin/com/lecture/special/demo/interfaces/LectureControllerTest.kt
@@ -0,0 +1,12 @@
+package com.lecture.special.demo.interfaces
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class LectureControllerTest {
+
+    @Test
+    fun apply() {
+    }
+}
