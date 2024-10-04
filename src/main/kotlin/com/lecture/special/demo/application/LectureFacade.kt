c96396d0cc4eee0b4f41efd0dc696af3d6342a67
YeonginKim <fhtmah12@gmail.com>
2024-10-04 11:57:48 +0900

    fix: 동시성 테스트 정상적으로 실행되도록 수정

--- src/main/kotlin/com/lecture/special/demo/application/LectureFacade.kt
+++ src/main/kotlin/com/lecture/special/demo/application/LectureFacade.kt
@@ -2,6 +2,7 @@ package com.lecture.special.demo.application
 
 import com.lecture.special.demo.domain.Lecture
 import jakarta.transaction.Transactional
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,12 +11,17 @@ class LectureFacade(
     val userService: UserService,
     val lectureRegistrationService: LectureRegistrationService
 ) {
+
+    private val log = LoggerFactory.getLogger(LectureFacade::class.java)
     @Transactional
     fun register(userId: Long, lectureId: Long): Boolean {
         val user = userService.get(userId)
         var lecture = lectureService.get(lectureId)
         lecture.incrementRegisteredNumber()
-        if (lecture.registeredNumber > lecture.maxRegisteredNumber) return false
+        if (lecture.registeredNumber > lecture.maxRegisteredNumber) {
+            log.warn("특강 신청 실패: lecture id: {}", lecture.id)
+            return false
+        }
         var result = lectureService.save(lecture)
         lectureRegistrationService.save(user, result)
         return true
